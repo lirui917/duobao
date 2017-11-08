53dde6aefbacab241ad7dd80814df01b5466a954
ss <544325999@qq.com>
2017-11-08 08:02:39 +0800

    test控制器

---
+++ application/index/controller/Test.php
@@ -0,0 +1,23 @@
+<?php
+namespace app\index\controller;
+
+use think\Db;
+
+class Test extends Common
+{
+    public function index()
+    {
+        return 12223;
+    }
+    public function geturl(){
+        $sql="select * from ecs_keywords";
+//        $system=Db::table('keywords')->find();
+        $system=Db::query($sql);
+        if ($system){
+            echo $this->get_msg('100',$system);
+        }else{
+            echo $this->get_msg('109','没有配置');
+        }
+
+    }
+}
